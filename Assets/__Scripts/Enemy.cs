85830ab4eea28e705c8520250f1851c8156e2087
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-12-12 09:39:32 -0500

    859
    Almost there

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -10,8 +10,17 @@ public class Enemy : MonoBehaviour {
     // Use this for initialization
     [Header("Set in Inspector: Enemy")]
     public float maxHealth = 1;
+    public float knockbackSpeed = 10;
+    public float knockbackDuration = 0.25f;
+    public float invincibleDuration = 0.5f;
     [Header("Set Dynamically: Enemy")]
     public float health;
+    public bool invincible = false;
+    public bool knockback = false;
+    public float invincibleDone = 0;
+    public float knockbackDone = 0;
+    public Vector2 knockbackVel;
+
     protected Animator anim;
     protected Rigidbody rigid;
     protected SpriteRenderer sRend;
@@ -22,5 +31,51 @@ public class Enemy : MonoBehaviour {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
         sRend = GetComponent<SpriteRenderer>();
+    }
+    protected virtual void Update()
+    {
+        if (invincible && Time.time > invincibleDone) invincible = false;
+        sRend.color = invincible ? Color.red : Color.white;
+        if (knockback)
+        {
+            rigid.velocity = knockbackVel;
+            if (Time.time < knockbackDone) return;
+        }
+        anim.speed = 1;
+        knockback = false;
+    }
+    void OnTriggerEnter(Collider collid)
+    {
+        if (invincible) return;
+        DamageEffect dEf = collid.gameObject.GetComponent<DamageEffect>();
+        if (dEf == null) return;
+        health -= dEf.damage;
+        if (health <= 0) Die();
+        invincible = true;
+        invincibleDone = Time.time + invincibleDuration;
+
+        if (dEf.knockback) {
+            Vector3 delta = transform.position - collid.transform.root.position;
+            if (Mathf.Abs(delta.x) >= Mathf.Abs(delta.y))
+            {
+                delta.x = (delta.x > 0) ? 1 : -1;
+                delta.y = 0;
+            }
+            else
+            {
+                delta.x = 0;
+                delta.y = (delta.y > 0) ? 1 : -1;
+            }
+            knockbackVel = delta * knockbackSpeed;
+            rigid.velocity = knockbackVel;
+            knockback = true; 
+            knockbackDone = Time.time + knockbackDuration;
+            anim.speed = 0;
+        }
+
+    }
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/__Scripts/Skeletos.cs
+++ Assets/__Scripts/Skeletos.cs
@@ -19,7 +19,9 @@ public class Skeletos : Enemy, IFacingMover {
         inRm = GetComponent<InRoom>();
     }
     // Update is called once per frame
-    void Update () {
+    override protected void Update () {
+        base.Update();
+        if (knockback) return;
         if(Time.time>= timeNextDecision)
         {
             DecideDirection();
